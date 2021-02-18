4916ccab922d5681dfb71d5e80569406d165385b
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-02-17 21:16:52 -0600

    Fixed data types for strict and more input stuff

--- mainfile.c
+++ mainfile.c
@@ -14,14 +14,14 @@ void readLineData();
 
 struct Data{
   int *serial;
-  string *product;
+  char *product;
   double *cost;
-  string *company;
+  char *company;
 };
 
-void int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) {
   struct Data theData;
-  readInputFile(theData);
+  readInputFile(&theData);
 
   return 0;
 }
@@ -30,21 +30,22 @@ void readInputFile(struct Data data){
   char line[100];
   FILE *file;
   file = fopen("items.txt", "r");  /* open the file for reading */
-  data->serial = malloc(100 * sizeof(int));
-  data->product = malloc(100 * sizeof(char) * 50);
-  data->cost = malloc(100 * sizeof(double));
-  data->company = malloc(100 * sizeof(char) * 20);
+  data.serial = malloc(100 * sizeof(int));
+  data.product = malloc(100 * sizeof(char) * 50);
+  data.cost = malloc(100 * sizeof(double));
+  data.company = malloc(100 * sizeof(char) * 20);
   int spot = 0;
   while(fgets(line, 100, file) != NULL){
-    *(data->serial + spot) = spot;
+    *(data.serial + spot) = spot;
 
     /* get a line, up to 100 chars from fr.  done if NULL */
-    sscanf(line);
+    scanf(line);
     readLineData(&line);
   }
   fclose(file);
 }
 
-void readLineData(struct Data data, char[] *line, int serial){
-    sscanf(line, "%i, %lf", *(data->cost+serial));
+void readLineData(struct Data data, char *line, int serial){
+    sscanf(line, "%i, %lf", *(data.cost+serial));
+    printf("%lf\n", *(data.cost+serial));
 }
