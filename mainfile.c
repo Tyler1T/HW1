96970fd47a936b62d0ef4b4ea0f4de58e30bd208
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-02-18 22:40:05 -0600

    Fixed cost data type to single precision and made it to the readLineData function but not through it

--- mainfile.c
+++ mainfile.c
@@ -15,14 +15,13 @@ void readLineData();
 struct Data{
   int *serial;
   char *product;
-  double *cost;
+  float *cost;
   char *company;
 };
 
 int main(int argc, char const *argv[]) {
   struct Data theData;
   readInputFile(&theData);
-
   return 0;
 }
 
@@ -32,20 +31,20 @@ void readInputFile(struct Data data){
   file = fopen("items.txt", "r");  /* open the file for reading */
   data.serial = malloc(100 * sizeof(int));
   data.product = malloc(100 * sizeof(char) * 50);
-  data.cost = malloc(100 * sizeof(double));
+  data.cost = malloc(100 * sizeof(float));
   data.company = malloc(100 * sizeof(char) * 20);
   int spot = 0;
   while(fgets(line, 100, file) != NULL){
     *(data.serial + spot) = spot;
-
+    printf(line);
     /* get a line, up to 100 chars from fr.  done if NULL */
-    scanf(line);
-    readLineData(&line);
+    readLineData(data, &line, spot);
   }
   fclose(file);
 }
 
 void readLineData(struct Data data, char *line, int serial){
+    printf("Made it to the readDataLine");
     sscanf(line, "%i, %lf", *(data.cost+serial));
     printf("%lf\n", *(data.cost+serial));
 }
