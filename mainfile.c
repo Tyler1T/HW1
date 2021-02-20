18b06933506a2e0676ebcee15f1dec7bb9722fb6
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-02-20 17:41:34 -0600

    Data input is working for first set

--- mainfile.c
+++ mainfile.c
@@ -9,39 +9,65 @@
   Program Description:
 */
 
-void readInputFile();
-void readLineData();
-
 struct Data{
   int *serial;
   char *product;
-  float *cost;
+  double *cost;
   char *company;
 };
 
+void readInputFile();
+
 int main() {
   struct Data theData;
-  theData.serial = calloc(100 ,  sizeof(int));
-  theData.product = calloc(100 ,  sizeof(char) * 50);
-  theData.cost = calloc(100 ,  sizeof(float));
-  theData.company = calloc(100 ,  sizeof(char) * 20);
+  theData.serial = malloc(100 * sizeof(int));
+  theData.product = malloc(100 * sizeof(char) * 50);
+  theData.cost = malloc(100 * sizeof(double));
+  theData.company = malloc(100 * sizeof(char) * 20);
   readInputFile(&theData);
+/*
+  printf("%d\n", theData.serial);
+  printf("%s\n", theData.product);
+  printf("$%.2f\n", theData.cost);
+  printf("%s\n", theData.company);*/
   return 0;
 }
 
 void readInputFile(struct Data *data){
-  char line[100];
+  char line[100]; //string buffer
   FILE *file;
-  file = fopen("items.txt", "r");  /* open the file for reading */
+  file = fopen("items.txt", "r");  //open the file for reading
+  int spot = 5;
+  fgets(line, 100, file);
+  fgets(line, 100, file);
+  fgets(line, 100, file);
+  fgets(line, 100, file);
+  fgets(line, 100, file);
 
-  int spot = 0;
-  /* get a line, up to 100 chars from fr.  done if NULL */
-  while(fgets(line, 100, file) != NULL){
+
+  char test[] = "$123.3";
+  float num=0;
+  sscanf(test, "$%lf", num);
+  printf("$%.2lf\n", num);
+  //scan in line and put each bit of data into the correct place in the struct
+  sscanf(line, "%d%*c\t%[^$]$%lf at %[^\n]", data->serial+spot,
+          data->product+spot, data->cost+spot, data->company+spot);
+  printf("%d\n", data->serial+spot);
+  printf("%s\n", data->product+spot);
+  printf("$%.2f\n", data->cost+spot);
+  printf("%s\n", data->company+spot);
+  //get a line, up to 100 chars from items.txt done if NULL
+  while(spot < 100){
+    fgets(line, 100, file);
     //scan in line and put each bit of data into the correct place in the struct
-    sscanf(line, "%s\t%[^\n]\t%lf\t%[^\n]", *(data->serial+spot), *(data->product+spot), *(data->cost+spot), *(data->company+spot));
+    sscanf(line, "%d%*c\t%[^$]$%lf at %[^\n]", &data->serial+spot,
+        data->product+spot, &data->cost+spot, data->company+spot);
     spot++;
-    printf("READ");
   }
+/*  printf("%d\n", data->serial);
+  printf("%s\n", data->product);
+  printf("$%.2f\n", data->cost);
+  printf("%s\n", data->company);*/
+
   fclose(file);
-  printf("%s\n", *(data->serial+12));
 }
