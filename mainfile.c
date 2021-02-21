9819b4baf3e5d78d666e8e084ea40a5162f0a61b
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-02-21 14:25:38 -0600

    Created shared memory for struct

--- mainfile.c
+++ mainfile.c
@@ -4,6 +4,19 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/ipc.h>
+#include<sys/wait.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#define QUEUE_NAME   "/addition"
+#define PERMISSIONS 0660
+#define MAX_MESSAGES 10
+#define MAX_MSG_SIZE 256
+#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
+
+#define MAX 9000000000 // 9 billion times
 
 /*
   Author: Tyler Tucker
@@ -24,9 +37,65 @@ void test();
 
 int main() {
   struct Data theData;
-  readInputFile(&theData);
-  int spot = 21;
-  test(&theData);
+
+  struct Data *dataPTR = &theData;
+  readInputFile(dataPTR);
+
+  printf("How many customers are there?");
+  for()
+
+  /* size (in bytes) of shared memory object */
+  const int SIZE = 10000;
+
+  /* name of the shared memory object */
+  const char *name = "Shared Data Pool";
+
+  int pid = fork();	// make two processes
+
+  /* shared memory file descriptor */
+  int shm_fd;
+
+  /* pointer to shared memory object */
+
+  if (pid < 0){	// fork failed
+    fprintf(stderr, "fork failed..\n");
+    exit(1);
+  }else if(pid == 0){ // Child process
+
+    /* Create or open a shared memory object */
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+
+    /* Set the shared memory object size */
+    ftruncate(shm_fd, SIZE);
+
+    /* Map the shared memory object into the current address space */
+    dataPTR = mmap(0, SIZE,  PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    //DO STUFF HERE
+
+  }else{	// Parent process
+
+    /* open the shared memory object */
+    shm_fd = shm_open(name, O_RDONLY, 0666);
+
+    wait(NULL);
+
+    /* memory map the shared memory object */
+    dataPTR = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+
+    //DO STUFF HERE
+
+    /* Unmap the shared memory */
+    munmap(dataPTR, SIZE);
+
+    /* Close the shared memory object */
+    close(shm_fd);
+
+    /* Delete the shared memory object */
+    shm_unlink(name);
+  }
+
+  //int spot = 21;
+  //test(&theData);
   //printf("%d %s $%.2lf %s\n", theData.serial[spot], theData.product[spot][5], theData.cost[spot], theData.company[spot]);
   return 0;
 }
@@ -55,7 +124,7 @@ void readInputFile(struct Data *data){
         tempP, &data->cost[i], tempC);
     data->product[i] = tempP;
     data->company[i] = tempC;
-    test(data, i);
+    //test(data, i);
     //printf("%d %s $%.2lf %s\n", data->serial[i], data->product[i], data->cost[i], data->company[i]);
   }
 
